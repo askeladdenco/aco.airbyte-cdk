6079e35e49f681dfc9424d78f36ef5be82bded78
tomasspangelo <tomas@spangelo.com>
2023-06-08 15:17:15 +0200

    Work in progress

--- airbyte-integrations/connectors/source-foodora/source_foodora/source.py
+++ airbyte-integrations/connectors/source-foodora/source_foodora/source.py
@@ -35,7 +35,7 @@ def get_vendors() -> list:
         },
     ]
 
-    return vendors
+    return (vendors, [{"globalEntityId": vendor["global_entity_id"], "vendorId": vendor['vendor_id']} for vendor in vendors])
 
 
 class FoodoraStream(HttpStream, ABC):
@@ -47,6 +47,8 @@ class FoodoraStream(HttpStream, ABC):
         self.username = config.get("username")
         self.password = config.get("password")
         self.start_date = config.get("initial_sync_date")
+        self.use_lookback_window = config.get("use_lookback_window")
+        self.lookback_window = config.get("lookback_window")
         self.vendors = get_vendors()
         self.access_token = None
         self.access_token_exp = pendulum.now()
@@ -56,6 +58,20 @@ class FoodoraStream(HttpStream, ABC):
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         return None
+
+    def backoff_time(self, response: requests.Response) -> Optional[float]:
+        """
+        Override this method to dynamically determine backoff time e.g: by reading the X-Retry-After header.
+
+        This method is called only if should_backoff() returns True for the input request.
+
+        :param response:
+        :return how long to backoff in seconds. The return value may be a floating point number for subsecond precision. Returning None defers backoff
+        to the default backoff behavior (e.g using an exponential algorithm).
+        """
+        self.update_access_token()
+        print("DENNE FUNKSJONEN HAR KJØRT!!!")
+        return float(0)
 
     def request_kwargs(
             self,
@@ -120,7 +136,7 @@ class OpeningHours(FoodoraStream):
     def request_body_json(
             self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> Mapping[str, Any]:
-        return {"vendors": self.vendors}
+        return {"vendors": self.vendors[0]}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         yield from response.json().get("vendors", [])
@@ -128,14 +144,11 @@ class OpeningHours(FoodoraStream):
 
 class Orders(FoodoraOrdersStream):
 
-    @property
-    def use_cache(self) -> bool:
-        return True
-
     def request_body_json(
             self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> Mapping[str, Any]:
         print("Next page token", next_page_token)
+        print("Stream slice", stream_slice)
         vendors = get_vendors()
         query = "query ListOrders($params: ListOrdersReq!) {\n  orders {\n    listOrders(input: $params) {\n      nextPageToken\n      resultTimestamp\n      orders {\n        ...OrderListingFields\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment OrderListingFields on OrderSummary {\n  orderId\n  globalEntityId\n  vendorId\n  vendorName\n  orderStatus\n  placedTimestamp\n  subtotal\n  billing {\n    commissionAmount\n    customerRefundGrossAmount\n    netRevenue\n    __typename\n  }\n  __typename\n}"
         pagination = {"pageSize": 50}
@@ -143,13 +156,13 @@ class Orders(FoodoraOrdersStream):
             pagination.update({"pageToken": next_page_token})
         return {"operationName": "ListOrders",
                 "variables": {"params":
-                              {"pagination": pagination, "timeFrom": str(stream_slice.get("from")), "timeTo": str(stream_slice.get("to")), "globalVendorCodes": [{"globalEntityId": "FO_NO", "vendorId": "n7iy"}, {"globalEntityId": "FO_NO", "vendorId": "u5iq"}, {"globalEntityId": "FO_NO", "vendorId": "zp04"}, {"globalEntityId": "FO_NO", "vendorId": "pv2d"}]}},
+                              {"pagination": pagination, "timeFrom": str(stream_slice.get("from")), "timeTo": str(stream_slice.get("to")), "globalVendorCodes": self.vendors[1]}},
                 "query": query}
 
     def stream_slices(
         self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
     ) -> Iterable[Optional[Mapping[str, Any]]]:
-        for start, end in self.generate_date_intervals(self.start_date):
+        for start, end in self.generate_date_intervals(self.get_start_date_sync()):
             yield {"from": start, "to": end}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
@@ -173,9 +186,12 @@ class Orders(FoodoraOrdersStream):
         next_page_token = data.get("listOrders", {}).get("nextPageToken")
         return next_page_token if next_page_token else None
 
+    def get_start_date_sync(self) -> pendulum.DateTime:
+        return pendulum.today().subtract(days=self.lookback_window) if self.use_lookback_window else pendulum.parse(self.start_date)
+
     @staticmethod
-    def generate_date_intervals(start_date: str, frequency=10):
-        start = pendulum.parse(start_date)
+    def generate_date_intervals(start_date: pendulum.DateTime, frequency: int = 10):
+        start = start_date
         delta = pendulum.duration(days=frequency)
         stop = pendulum.today().add(days=1)
 
@@ -208,43 +224,13 @@ class OrderDetails(HttpSubStream, FoodoraOrdersStream):
                                    "placedTimestamp": "2023-06-06T09:20:52.000Z", "isBillingDataFlagEnabled": False}},
                 "query": query}
 
-    def stream_slices(
-        self, *, sync_mode: SyncMode, cursor_field: List[str] = None, stream_state: Mapping[str, Any] = None
-    ) -> Iterable[Optional[Mapping[str, Any]]]:
-        for start, end in self.generate_date_intervals(self.start_date):
-            yield {"from": start, "to": end}
-
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         print(response.url)
-        data = response.json().get("data", {}).get("orders", {})
-        if not data:
-            orders = []
-        else:
-            orders = data.get("listOrders", {}).get("orders", [])
-        print(orders)
-        self.num_orders += len(orders)
-        print(f"Num orders: {self.num_orders}")
-        yield from orders
+        data = response.json().get("data", {}).get("orders", {}).get("order", {})
+        yield data
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
-
-        data = response.json().get("data", {}).get(
-            "orders", {})
-        if data is None:
-            return None
-        next_page_token = data.get("listOrders", {}).get("nextPageToken")
-        return next_page_token if next_page_token else None
-
-    @staticmethod
-    def generate_date_intervals(start_date: str, frequency=10):
-        start = pendulum.parse(start_date)
-        delta = pendulum.duration(days=frequency)
-        stop = pendulum.today().add(days=1)
-
-        while start < stop:
-            stop = min(start + delta, stop)
-            yield start, stop
-            start = stop
+        return None
 
 
 class SourceFoodora(AbstractSource):
